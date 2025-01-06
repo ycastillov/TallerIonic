3ba425981d1ec9f643c0a3165f170bb82469e58e
yamirCastilloV <166874609+yamirCastilloV@users.noreply.github.com>
2025-01-05 21:21:57 -0300

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.tallerionic',
+  appName: 'tallerionic',
+  webDir: 'www'
+};
+
+export default config;
